60b7afcb12977c9791dea8cb86133a2937245869
Narongrit <s5830213040@phuket.psu.ac.th>
2018-12-01 04:24:24 +0700

    date picker

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/// Widgets tabs -->
+$(function () {
+    $("#tabs").tabs();
+    $("#datepicker").datepicker();
+    $('input[name="daterange"]').daterangepicker({
+        opens: 'left'
+      }, function(start, end, label) {
+        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+      });
+});
+
